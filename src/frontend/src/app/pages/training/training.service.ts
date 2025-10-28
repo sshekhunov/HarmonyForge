79564abdc77b586cf20c6762174fe4796c3fdfac
Sergey Shehunov <sergey.shehunov@outlook.com>
2025-10-28 17:04:52 +0400

    Adds environment.ts

--- src/frontend/src/app/pages/training/training.service.ts
+++ src/frontend/src/app/pages/training/training.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { HarmonyAnalysisRequest, HarmonyAnalysisResponse } from './training.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrainingService {
-  private readonly apiUrl = 'http://localhost:5065/api/harmony/harmonyanalysis';
+  private readonly apiUrl = `${environment.apiBaseUrl}/api/harmony/harmonyanalysis`;
 
   constructor(private http: HttpClient) { }
 
