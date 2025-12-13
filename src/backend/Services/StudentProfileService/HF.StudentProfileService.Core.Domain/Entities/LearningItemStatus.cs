8601b05a0f43dc3e5e05855468b57af9c08f1aa0
Sergey Shehunov <sergey.shehunov@outlook.com>
2025-12-13 17:46:52 +0400

    Adds StudentProfileEntities

---
+++ src/backend/Services/StudentProfileService/HF.StudentProfileService.Core.Domain/Entities/LearningItemStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace HF.StudentProfileService.Core.Domain.Entities;
+
+public class LearningItemStatus: BaseEntity
+{
+    public Student Student { get; set; } = null!;
+
+    public Guid LearningItemId { get; set; }
+
+    public LearningItemType LearningItemType { get; set; }
+
+    public DateTime DateTime { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public double? Score { get; set; }
+}
+
+public enum LearningItemType
+{    
+    Article,
+    Excercise,
+    Test
+}
