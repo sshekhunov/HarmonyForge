feb359683c697815d2f4b7645f047bd66450d26a
Sergey Shehunov <sergey.shehunov@outlook.com>
2026-02-03 15:46:15 +0400

    Removes top bar menu item

--- src/frontend/src/app/layout/component/app.topbar.ts
+++ src/frontend/src/app/layout/component/app.topbar.ts
@@ -42,11 +42,6 @@ import { AuthService } from '../../shared/services/auth.service';
                     </a>
                 </li>
                 <li>
-                    <a (click)="router.navigate(['/training'])" pRipple class="px-0 py-4 text-surface-900 dark:text-surface-0 font-medium text-xl">
-                        <span>Тренажер</span>
-                    </a>
-                </li>
-                <li>
                     <a (click)="router.navigate(['/theory'])" pRipple class="px-0 py-4 text-surface-900 dark:text-surface-0 font-medium text-xl">
                         <span>Теория</span>
                     </a>
